9adcf8aa34fa068726ab7e693c7772d6f282395d
KITA-DS12 <kita.ds12@gmail.com>
2023-02-06 23:15:27 +0900

    add package && init pages

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,4 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import ElementPlus from "element-plus"
+import "element-plus/theme-chalk/index.css"
+import axios from "axios"
+
+axios.defaults.baseURL = 'http://localhost:8888'
+axios.defaults.withCredentials = true
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(ElementPlus)
+
+app.mount("#app");
