d34ef67278b891b1be7c0fa6a2e8f854c262f409
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2023-02-12 00:23:45 +0000

    その他フォルダーの名前変更の対応

--- server/controllers.py
+++ server/controllers.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Optional
 import time
 import shutil
 import os
@@ -25,6 +25,7 @@ app.add_middleware(
 class Post(BaseModel):
     bookmark : str = Field("Html", description="Base64のHTML")
     folder : List[str] = Field(["Folder"], description="フォルダー")
+    other : str = Field("その他", description="その他のフォルダーの名前")
 
 @app.post(
     "/html-json"
@@ -44,10 +45,13 @@ async def upload(bookmark_file: Post):
     bookmarks.convert("json")
     os.remove(path)
 
+
+
     bookmark_json = BookMark_Json(bookmarks.bookmarks)
     categorize_list = mf(
         book_mark_info_list= bookmark_json.folder_to_list(),
         candidate_labels_list= bookmark_file.folder,
+        other_folder_name=bookmark_file.other
         
     )
     bookmark = bookmark_json.list_to_folder(
